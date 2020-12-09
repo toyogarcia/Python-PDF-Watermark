c237f84aeb3ae510421341ceb10092276d2cdab3
Garcia Aranda <eulogio.garcia@unisys.com>
2020-12-09 17:39:13 +0100

    Added try/except block

--- watermark.py
+++ watermark.py
@@ -1,17 +1,26 @@
 import PyPDF2
 import sys
 
-watermark = PyPDF2.PdfFileReader(open (sys.argv[1],'rb'))
-template =  PyPDF2.PdfFileReader(open (sys.argv[2],'rb'))
-output = PyPDF2.PdfFileWriter()
+try:
+    if len(sys.argv) < 4:
+        print('Wrong number of parameters')
+        sys.exit()
+        
+    watermark = PyPDF2.PdfFileReader(open (sys.argv[1],'rb'))
+    template =  PyPDF2.PdfFileReader(open (sys.argv[2],'rb'))
+    output = PyPDF2.PdfFileWriter()
 
-for i in range(template.getNumPages()):
-    page = template.getPage(i)
-    page.mergePage(watermark.getPage(0))
-    output.addPage(page)
+    for i in range(template.getNumPages()):
+        page = template.getPage(i)
+        page.mergePage(watermark.getPage(0))
+        output.addPage(page)
 
-with open (sys.argv[3],'wb') as file:
-    output.write(file)
+    with open (sys.argv[3],'wb') as file:
+        output.write(file)
+
+except FileNotFoundError:
+    print('Error in parameters')
+
     
 
 
